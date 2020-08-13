db80ec457bbcc194bcd5a408d77fc05a80d394a0
Michael Lee <mikelee0330@gmail.com>
2020-08-13 00:25:21 -0700

    create react app and express server

--- socket-io-server/routes/index.js
+++ socket-io-server/routes/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
+  res.send({ response: "Express response" }).status(200);
+});
+
+router.get("/test", (req, res) => {
+  res.send({ response: "Test response"}).status(200);
 });
 
 module.exports = router;
